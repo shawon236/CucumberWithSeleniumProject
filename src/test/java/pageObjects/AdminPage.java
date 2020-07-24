3ad23e6d1ffcabb6d809d6e045c5c4f96fdac53a
shawon236 <shawon236@yahoo.com>
2020-07-24 10:42:02 -0400

    New changes from clone branch to developbranch

--- src/test/java/pageObjects/AdminPage.java
+++ src/test/java/pageObjects/AdminPage.java
@@ -140,7 +140,7 @@ public class AdminPage {
         userName.clear();
         userName.sendKeys(userNam);
     }
-
+    //User select status from list
     public void SelectStatus(String status){
         Select optionSelect = new Select(statusSelect);
         optionSelect.selectByVisibleText(status);
